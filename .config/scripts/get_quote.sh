328202e55371b2c923d2bec73d4321777a45c435
oshitaka <oshitaka@users.noreply.github.com>
2025-02-01 21:09:08 +0600

    shorten quote length

--- .config/scripts/get_quote.sh
+++ .config/scripts/get_quote.sh
@@ -10,14 +10,16 @@ yellow='\033[1;33m'
 # URL of the webpage
 URL="https://altl.io/"
 
-# Use curl to fetch the webpage content
-CONTENT=$(curl -s "$URL")
+QUOTE="This is a very long string that is greater than 100 characters in length and will cause the loop to continue executing until it is shortened to 100 characters or less."
 
-# Use grep to extract the quote text and author
-QUOTE=$(echo "$CONTENT" | xmllint --html --xpath "//div[@class='quote' and @id='quoteText']/text()" - 2>/dev/null)
-
-# Remove whitespace characters from the output
-QUOTE=$(echo "$QUOTE" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+while [ ${#QUOTE} -gt 100 ]; do
+	# Use curl to fetch the webpage content
+	CONTENT=$(curl -s "$URL")
+	# Use grep to extract the quote text and author
+	QUOTE=$(echo "$CONTENT" | xmllint --html --xpath "//div[@class='quote' and @id='quoteText']/text()" - 2>/dev/null)
+	# Remove whitespace characters from the output
+	QUOTE=$(echo "$QUOTE" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+done
 
 # Print the quote and author
 echo $QUOTE
