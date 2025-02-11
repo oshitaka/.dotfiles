bc25e16971a3bef2cfeee5fc84b72c29426ba808
oshitaka <oshitaka@users.noreply.github.com>
2025-02-11 12:06:48 +0600

    added power profile, changed style of waybar, added bash aliases

--- .config/scripts/vpn.sh
+++ .config/scripts/vpn.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 ip=$(curl -s ifconfig.me)
-if [[ -d "/proc/sys/net/ipv4/conf/tun0" || -d "/proc/sys/net/ipv4/conf/wg0" ]]; then
+if [[ $ip == "206.188.196.102" ]]; then
+#if [[ -d "/proc/sys/net/ipv4/conf/tun0" || -d "/proc/sys/net/ipv4/conf/vpn0" || -d "/proc/sys/net/ipv4/conf/wg0" ]]; then
   # If the IP matches, set the color to green
   echo "{\"text\": \"$ip 󰌾\", \"class\": \"ip-match\", \"tooltip\":\"secure\"}"
 elif [[ $ip == "" ]]; then
